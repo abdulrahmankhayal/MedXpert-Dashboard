af24445c49d6c7e3744cdde1bbf3a4ae25e01f03
abdulrahmankhayal <50086316+abdulrahmankhayal@users.noreply.github.com>
2022-09-26 13:44:26 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+import dash
+from dash import  html, Output, Input, State
+import dash_labs as dl
+import dash_bootstrap_components as dbc
+
+
+
+
+app = dash.Dash(
+    __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.BOOTSTRAP],meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}]
+)
+
+
+offcanvas = html.Div(
+    [
+        # Banner
+ html.Div(
+            id="banner",
+            className="banner",
+            children=[html.Img(src=app.get_asset_url("Logo.png"))],
+        ),dbc.Button("Explore", id="open-offcanvas", n_clicks=0),
+        dbc.Offcanvas(
+            dbc.ListGroup(
+                [
+                    dbc.ListGroupItem(page["name"], href=page["path"])
+                    for page in dash.page_registry.values()
+                    if page["module"] != "pages.not_found_404"
+                ]
+            ),
+            id="offcanvas",
+            is_open=False,
+        )  
+    ]
+
+    ,
+    className="banner"
+)
+
+app.layout = dbc.Container(
+    [offcanvas, dl.plugins.page_container],
+    fluid=True,
+)
+
+@app.callback(
+    Output("offcanvas", "is_open"),
+    Input("open-offcanvas", "n_clicks"),
+    [State("offcanvas", "is_open")],
+)
+def toggle_offcanvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8001)
